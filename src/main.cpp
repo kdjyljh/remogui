0db91cd08ad1e7961adbaad9604951ccb1996c12
JiangHua Luo <JiangHua Luo>
2017-11-30 20:24:14 +0800

    successfully merge two branches

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@
 static void threadfunc();
 int main(int argc, char *argv[])
 {
+    CommProtoVariables::Get()->request_version(COMMDEVICE_CAMERA);
+    CommProtoVariables::Get()->request_status(COMMDEVICE_TRIOPHEAD);
+
     boost::thread threadlocal(threadfunc);
 
     QApplication a(argc, argv);
@@ -20,19 +23,19 @@ int main(int argc, char *argv[])
 }
 
 static void threadfunc() {
-    std::unordered_map<std::string, boost::function<void (void)>> mapName2Func;
-    mapName2Func["CAMERA_PING"] = boost::bind(&CommProtoVariables::request_ping, CommProtoVariables::Get(), COMMDEVICE_CAMERA);
-    mapName2Func["TRIOPHEAD_PING"] = boost::bind(&CommProtoVariables::request_ping, CommProtoVariables::Get(), COMMDEVICE_TRIOPHEAD);
-    mapName2Func["PC_PING"] = boost::bind(&CommProtoVariables::request_ping, CommProtoVariables::Get(), COMMDEVICE_PC);
-    mapName2Func["CAMERA_VERSION"] = boost::bind(&CommProtoVariables::request_version, CommProtoVariables::Get(), COMMDEVICE_CAMERA);
-    mapName2Func["TRIOPHEAD_VERSION"] = boost::bind(&CommProtoVariables::request_version, CommProtoVariables::Get(), COMMDEVICE_TRIOPHEAD);
-    mapName2Func["PC_VERSION"] = boost::bind(&CommProtoVariables::request_version, CommProtoVariables::Get(), COMMDEVICE_PC);
-    mapName2Func["CAMERA_STATUS"] = boost::bind(&CommProtoVariables::request_status, CommProtoVariables::Get(), COMMDEVICE_CAMERA);
-    mapName2Func["TRIOPHEAD_STATUS"] = boost::bind(&CommProtoVariables::request_status, CommProtoVariables::Get(), COMMDEVICE_TRIOPHEAD);
-    mapName2Func["PC_STATUS"] = boost::bind(&CommProtoVariables::request_status, CommProtoVariables::Get(), COMMDEVICE_PC);
-    for (auto &&pair : mapName2Func) {
-      int intervalUS = 1e6; // 1s
-      TimedTask::Get()->work_irrevocable(pair.first, pair.second, intervalUS*mapName2Func.size());
-      boost::this_thread::sleep_for(boost::chrono::microseconds(intervalUS));
-    }
+//    std::unordered_map<std::string, boost::function<void (void)>> mapName2Func;
+//    mapName2Func["CAMERA_PING"] = boost::bind(&CommProtoVariables::request_ping, CommProtoVariables::Get(), COMMDEVICE_CAMERA);
+//    mapName2Func["TRIOPHEAD_PING"] = boost::bind(&CommProtoVariables::request_ping, CommProtoVariables::Get(), COMMDEVICE_TRIOPHEAD);
+//    mapName2Func["PC_PING"] = boost::bind(&CommProtoVariables::request_ping, CommProtoVariables::Get(), COMMDEVICE_PC);
+//    mapName2Func["CAMERA_VERSION"] = boost::bind(&CommProtoVariables::request_version, CommProtoVariables::Get(), COMMDEVICE_CAMERA);
+//    mapName2Func["TRIOPHEAD_VERSION"] = boost::bind(&CommProtoVariables::request_version, CommProtoVariables::Get(), COMMDEVICE_TRIOPHEAD);
+//    mapName2Func["PC_VERSION"] = boost::bind(&CommProtoVariables::request_version, CommProtoVariables::Get(), COMMDEVICE_PC);
+//    mapName2Func["CAMERA_STATUS"] = boost::bind(&CommProtoVariables::request_status, CommProtoVariables::Get(), COMMDEVICE_CAMERA);
+//    mapName2Func["TRIOPHEAD_STATUS"] = boost::bind(&CommProtoVariables::request_status, CommProtoVariables::Get(), COMMDEVICE_TRIOPHEAD);
+//    mapName2Func["PC_STATUS"] = boost::bind(&CommProtoVariables::request_status, CommProtoVariables::Get(), COMMDEVICE_PC);
+//    for (auto &&pair : mapName2Func) {
+//      int intervalUS = 1e6; // 1s
+//      TimedTask::Get()->work_irrevocable(pair.first, pair.second, intervalUS*mapName2Func.size());
+//      boost::this_thread::sleep_for(boost::chrono::microseconds(intervalUS));
+//    }
 }
